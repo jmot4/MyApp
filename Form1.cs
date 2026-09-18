f14a4c7618ffcfaf4ced8f257934bd640dfd8261
jmot4 <jmota@gtech-services.com>
2026-09-17 22:52:21 -0600

    Ejemplo 1 de CSV

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CsvHelper;
+using System.Globalization;
+using System.IO;
 
 namespace MyApp
 {
     public partial class Form1 : Form
     {
+        List<Persona> registros = new List<Persona>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            if (ofdCSV.ShowDialog() == DialogResult.OK)
+            {
+                var reader = new StreamReader(ofdCSV.FileName);
+                var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                registros = csv.GetRecords<Persona>().ToList();
+                foreach (var registro in registros) {
+                    dgvRegistros.Rows.Add(registro.id, registro.name, registro.email);
+                }
+            }
+
         }
     }
 }
